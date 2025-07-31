ffdd87c7ed7c1c557dee6a347a1ca0446e3e6d54
Nguyen Hai <nguyenhaipc98@gmail.com>
2025-07-31 16:07:51 +0300

    update inter_2 & created interview_3

--- interview_2/Django_project_test/myapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Item
-
-@admin.register(Item)
-class ItemAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'description')
-    list_filter = ('name',)
-    search_fields = ('name', 'description')
-    ordering = ('id',)
